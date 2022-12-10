b4de06fcb7f1858799ec7881c96219a548e20259
RowanDahroug591 <rown192823@bue.edu.eg>
2022-12-11 00:15:10 +0200

    ew

--- CarFandomAPI/repairs-demo/src/Components/repairs/Repairpage.js
+++
@@ -1,9 +0,0 @@
-import {useState} from 'react';
-import Repairlist from '../repairs/Repairlist'
-const Repairpage=()=> {
-    const [repair, setrepair]=useState();
-    return <div>
-        <Repairlist repair={repair}/>
-    </div>
-};
-export default Repairpage;
--- CarFandomAPI/repairs-demo/src/UI/CardBody.js
+++
@@ -1,8 +0,0 @@
-const CardBody=(props) =>{
-    return(
-        <div className="flex flex-col justiy-center items-center gap-2">
-            {props.children}
-        </div>
-    );
-};
-export default CardBody;
