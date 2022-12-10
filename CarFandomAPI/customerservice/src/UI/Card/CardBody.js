54e0980e93a5e345468ea9188d07eb329b0b1eee
reemf011 <85066659+reemf011@users.noreply.github.com>
2022-12-10 22:46:35 +0300

    ree edit

--- CarFandomAPI/customerservice/src/UI/Card/CardBody.js
+++
@@ -1,9 +0,0 @@
-const CardBody = (props) => {
-    return(
-        <div className='flex items-col justify-center items-center gap-2'>
-            <h1>hello!</h1>
-        </div>
-
-    );
-};
-export default CardBody;
