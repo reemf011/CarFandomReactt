1fa0cf6d676497acfc3da1d0677019f9e16a9494
reemf011 <85066659+reemf011@users.noreply.github.com>
2022-12-10 21:39:44 +0300

    Delete cars-demo directory

--- cars-demo/src/UI/Card/Card.js
+++
@@ -1,9 +0,0 @@
-const Card = (props) => {
-    return (
-        <div className="flex flox-col items-center gap-8 rounded-xl bg-slate-200 w-[500px] aspect-square overflow-hidden">
-            {props.children}
-            </div>
-    );
-};
-
-export default Card;
