1e5ef0384daa3530f634cd6cdcf5f3665e978c7a
RowanDahroug591 <rown192823@bue.edu.eg>
2022-12-11 14:36:13 +0200

    feedback

--- CarFandomReact/src/components/customer/CustomerProfile.js
+++ CarFandomReact/src/components/customer/CustomerProfile.js
@@ -2,7 +2,7 @@ import CustomerSummary from "./CustomerProfile";
 
 const CustomerProfile =(props) => {
     return(
-        <div className= 'grid grid-cols-2 gap-5 justify-center items-center'>
+        <div className= "grid grid-cols-2 gap-5 justify-center items-center">
           {props.CustomerProfile.map((c) =>(
             <CustomerProfile Customer={c} key={props.ID}/>
           ))}
